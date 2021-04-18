ac7d1c66080f87dd1a849580f04aac917f806c5b
wangju <wangju@shein.com>
2021-04-18 17:45:50 +0800

    lib and unit test

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(kitty_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use kitty_os::println;
 
-#[macro_use]
-extern crate lazy_static;
 
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic_info(info: &PanicInfo) -> ! {
     println!("{}",info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kitty_os::test_panic_handler(info);
+}
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("hello world! ");
-    panic!("Some panic message");
+
+    #[cfg(test)] test_main();
+
     loop {}
 }
+
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,8 @@
 use volatile::Volatile;
 use core::fmt;
 use spin::Mutex;
+#[cfg(test)]
+use crate::{serial_print, serial_println};
 
 
 // vga 模式下屏幕的宽和高
@@ -14,8 +16,6 @@ lazy_static!(
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 );
-
-
 
 #[repr(transparent)]
 struct Buffer {
@@ -161,4 +161,27 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_many() {
+    serial_print!("test_println_many... ");
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output() {
+    serial_print!("test_println_output... ");
+
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]");
 }
